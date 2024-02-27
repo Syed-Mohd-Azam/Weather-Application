d02f8f7c493014a3e2ef5d25d769834348351def
Syed-Mohd-Azam <smazamamu@gmail.com>
2024-02-27 17:20:04 +0530

    API_ID and WEATHER_CITY_API created and stored

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,4 @@
-export const API_KEY = "fcbd9181257065e6a01b65e33080f80e";
+const API_KEY = "fcbd9181257065e6a01b65e33080f80e";
+export const WEATHER_CITY_API =
+  "https://api.openweathermap.org/data/2.5/weather?q=";
+export const APP_ID = "&appid=" + API_KEY;
