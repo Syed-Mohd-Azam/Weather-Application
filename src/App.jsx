985d84304f4e2f9136b1e4f2d5f07946b2ca7774
Syed-Mohd-Azam <smazamamu@gmail.com>
2024-02-27 17:43:46 +0530

    Backend API called and implemented to store the weather Data

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
 import Search from "./components/Search";
 import WeatherInformation from "./components/WeatherInformation";
 import ExtraInformation from "./components/ExtraInformation";
+import { useState } from "react";
+import { useEffect } from "react";
+import { APP_ID, WEATHER_CITY_API } from "./utils/constants";
 function App() {
+  const [weatherInfo, setWeatherInfo] = useState([]);
+  const [city, setCity] = useState("");
+  useEffect(() => {
+    fetchWeatherData(city);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [city]);
+  const fetchWeatherData = async (city) => {
+    const weatherData = await fetch(WEATHER_CITY_API + city + APP_ID);
+    const jsonData = await weatherData.json();
+    setWeatherInfo(jsonData);
+  };
+  if (weatherInfo?.length === 0) {
+    return null;
+  }
   return (
     <>
       <section
@@ -10,7 +27,7 @@ function App() {
           backgroundImage: "url(/src/assets/Background.jpg)",
         }}
       >
-        <Search />
+        <Search city={city} setCity={setCity} />
         <WeatherInformation />
         <ExtraInformation />
       </section>
